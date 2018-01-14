4de8d2a8b063d46fbbc190e0726d435fa7df70a3
Evgeniy Vashchuk <evgeniy.vashchuk.mail@gmail.com>
2018-01-14 15:08:45 +0200

    first homework - create 3 objects

---
+++ 1. 12.01.2018/common.js
@@ -0,0 +1,37 @@
+// Home Work #1 (12.01.2018)
+
+// Object - 1
+
+var xiaomi = {};
+
+xiaomi.model = 'Redmi 4X';
+xiaomi.operatingSystem = 'Android 6.0';
+xiaomi.weight = 150;
+
+xiaomi.playMusic = function() {
+	alert('Music plays!');
+};
+
+// Object - 2
+
+var meizu = {
+	model : 'M3 Note',
+	operatingSystem : 'Android 5.1',
+	weight : 163,
+
+	call: function() {
+		alert( 'The call was made!' );
+	}
+};
+
+// Object - 3
+
+var apple = {
+	model : 'iPhone X',
+	operatingSystem : 'iOS 11',
+	weight : 174,
+
+	MakePhoto: function() {
+		alert( 'The photo was made!' );
+	}
+};
